6c67a15458d103f934bf7b0558243622942899f1
Gor <7495500@gmail.com>
2021-02-17 00:14:08 +0300

    Add storage configuration and gem mini_magick

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
+
+  add_one_attached :avatar
+
+  def avatar_thumbnail
+    avatar.variant(resize: '150x150!').processed
+  end
+
 end
