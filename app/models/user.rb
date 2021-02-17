11830a910629b0051f83a4aa4092d215102f65ed
Gor <7495500@gmail.com>
2021-02-17 03:02:54 +0300

    Add snippet for default_images

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,29 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: [ :create, :update ]
 
   def avatar_thumbnail
-    avatar.variant(resize: '150x150!').processed
+    if avatar.attached?
+      avatar.variant(resize: '150x150!').processed
+    else
+      '/default_person.jpg'
+    end
+  end
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_person.jpg'
+          )
+        ), filename: 'default_person.jpg',
+        content_type: 'image/jpg'
+      )
+    end
   end
 
 end
